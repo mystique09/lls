52ad9cdfbd3bc164879bc2ae0056000923abfe37
mystique09 <knightkrypto7@gmail.com>
2022-03-11 17:26:23 +0800

    fix: bug nested folder overlapping

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use std::{
 };
 
 fn main() {
-    let mut str_format = format!(".");
+    //let mut str_format = format!("{}\n", );
     let mut depth = 0;
     let f_path = String::from("src/");
+    let mut str_format = format!("{}\n", f_path);
     //str_format.push_str(&" ----".repeat(depth)); //.push_str("└──");
     read_f(f_path, &mut str_format, &mut depth);
     println!("{}", str_format);
@@ -42,13 +43,8 @@ fn format_f(
     depth: &mut usize,
 ) {
     if ftype.is_dir() {
-        let mut inner_depth = 2 * *depth;
+        let mut inner_depth = *depth;
         let mut inner_fstr = String::from(format!("\n{}└── {}", &" ".repeat(inner_depth), fname));
-        //println!("{}", inner_fstr);
-        //fstr.push_str(&format!("\n", &" ".repeat(inner_depth), inner_fstr).into_boxed_str());
-        //fstr.push_str(&inner_fstr.to_string());
-        //println!("├── {}", fname);
-        //println!("{}/{}", fpath, fname);
         read_f(
             format!("{}/{}", fpath, fname),
             &mut inner_fstr,
@@ -58,5 +54,4 @@ fn format_f(
     } else if ftype.is_file() {
         fstr.push_str(&format!("\n{}└── {}", &" ".repeat(*depth), fname).into_boxed_str());
     }
-    //println!("{}", fstr.len());
 }
