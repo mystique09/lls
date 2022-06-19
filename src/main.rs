9d12e7ac64e16f3cba7318bf134a6511d046ecee
mystique09 <knightkrypto7@gmail.com>
2022-06-19 16:31:52 +0800

    add: <option> argument, whether to include hidden files and folders when listing a certain root folder. feat: better help prompt.

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,39 @@ use std::{
 
 fn main() {
     let mut depth = 0;
-    let mut flags: Vec<String> = vec![];
-    for flag in args() {
-        flags.push(flag);
-    }
+    let mut flags = args().collect::<Vec<_>>();
     flags.remove(0);
 
-    if flags.len() > 1 {
+    if flags.len() > 2 {
         eprintln!("\u{001b}[31mToo many arguments.\u{001b}[0m");
         return;
     }
 
     let command = flags.get(0).or(Some(&".".to_string())).unwrap().to_string();
+    let all = { command == "-a" || command == "--all" };
 
-    if command == *"help" {
-        println!("\u{001b}[32mUsage: lls <path>\u{001b}[0m");
+    if command == *"--help" || command == *"-h" {
+        println!("\u{001b}[34mUsage: \n\tlls <option> <path> \n\nOption: \n\t-a: include hidden files and folders.\u{001b}[0m");
         return;
     }
+
+    if flags.len() == 2 {
+        if !command.contains("-") {
+            eprintln!(
+                "\u{001b}[31mInvalid argument: {}\u{001b}[0m",
+                flags.get(1).unwrap()
+            );
+            return;
+        }
+    }
+
+    let command = {
+        if all {
+            flags.get(1).or(Some(&".".to_string())).unwrap().to_string()
+        } else {
+            command
+        }
+    };
 
     let str_format = command.to_string();
 
@@ -30,12 +46,12 @@ fn main() {
     let mut total_fs = 0;
 
     println!("\u{001b}[34m{}\u{001b}[0m", str_format);
-    read_f(command, &mut depth, &mut total_fs, &mut total_dirs);
+    read_f(command, &mut depth, &mut total_fs, &mut total_dirs, all);
 
     println!("{} directorie(s), {} file(s)", total_dirs, total_fs);
 }
 
-fn read_f(fpath: String, depth: &mut usize, tfs: &mut usize, tds: &mut usize) {
+fn read_f(fpath: String, depth: &mut usize, tfs: &mut usize, tds: &mut usize, all: bool) {
     let dirs = read_dir(&fpath);
     *depth += 1;
 
@@ -51,6 +67,7 @@ fn read_f(fpath: String, depth: &mut usize, tfs: &mut usize, tds: &mut usize) {
                             depth,
                             tfs,
                             tds,
+                            all,
                         ),
                         Err(why) => eprintln!("\u{001b}[31m{}\u{001b}[0m", why),
                     },
@@ -69,8 +86,18 @@ fn format_f(
     depth: &mut usize,
     tfs: &mut usize,
     tds: &mut usize,
+    all: bool,
 ) {
     if ftype.is_dir() {
+        // return if the directory is hidden
+        if fname.starts_with(".") && !all {
+            return;
+        }
+
+        if fname.starts_with(".git") {
+            return;
+        }
+
         *tds += 1;
         let mut inner_depth = *depth;
         let inner_fstr = format!(
@@ -80,8 +107,19 @@ fn format_f(
         );
 
         println!("{}", &inner_fstr);
-        read_f(format!("{}/{}", fpath, fname), &mut inner_depth, tfs, tds);
+        read_f(
+            format!("{}/{}", fpath, fname),
+            &mut inner_depth,
+            tfs,
+            tds,
+            all,
+        );
     } else if ftype.is_file() {
+        // return if the file is hidden
+        if fname.starts_with(".") && !all {
+            return;
+        }
+
         *tfs += 1;
         println!("{}└── {}", &" ".repeat(*depth), fname);
     }
